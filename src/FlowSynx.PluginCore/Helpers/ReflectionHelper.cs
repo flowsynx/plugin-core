057b068959b8f2e2bd4196c49a16c429f702a94a
ziagham <amin.ziagham@outlook.com>
2025-05-20 20:08:59 +0200

    Add a helper for avoid and prevent calling methods via reflection for plugins #7

---
+++ src/FlowSynx.PluginCore/Helpers/ReflectionHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+namespace FlowSynx.PluginCore.Helpers;
+
+public static class ReflectionHelper
+{
+    public static bool IsCalledViaReflection()
+    {
+        var stack = new StackTrace();
+        var frames = stack.GetFrames();
+
+        if (frames == null)
+            return false;
+
+        foreach (var frame in frames)
+        {
+            var callingMethod = frame.GetMethod();
+            if (callingMethod == null) continue;
+
+            if (callingMethod.DeclaringType?.Namespace?.StartsWith("System.Reflection") == true)
+                return true;
+        }
+
+        return false;
+    }
+}
